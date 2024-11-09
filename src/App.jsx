ba34238d46fa99d6dd1212f31de32981b2935a60
Sven Andersson <sven@andersson.es>
2024-11-09 14:55:40 +0100

    fix vite logo problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
